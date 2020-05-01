5d9bb669764e754172c48d00ee270ec91076bb50
Farahnajjar99 <farahnajjar888@gmail.com>
2020-05-01 10:04:50 +0300

    my cake

--- W4D6/W4D6.js
+++ W4D6/W4D6.js
@@ -4,6 +4,16 @@ and returns an object with 3 key-value pairs.
 The names of each property and their respective values are up to you, 
 but the values cannot be undefined. How would you describe your favorite dessert?
 
+function favoriteDessert(name,price,flavor){
+    return {
+        name:name,
+        price:price,
+        flavor:flavor
+    }
+}
+
+var customer=favoriteDessert(name:"Rainbow Cake",price:22,flavor:"chocolate");
+
 For example, one could return an object such as:
 
 { name: "Flan", ovenTemp: 350, prepTime: "25 minutes + 50 minutes chilling" }
